77fd7503aff44ff4466e1c3c2cfc26980cf0cc3a
Fernando <fernando@technosoftware.com.br>
2015-11-05 10:23:42 -0200

    Refatoração

--- install-jboss.sh
+++ install-jboss.sh
@@ -1,12 +1,12 @@
 # Jboss - instalacao, permissao e incializacao do mesmo
 cd /tmp
-sudo wget https://raw.githubusercontent.com/technsoftware/technosoftware-install-infra/master/wildfly-install.sh
+sudo wget https://raw.githubusercontent.com/technosoftware/ts-install-infra/master/wildfly-install.sh
 dos2unix /tmp/wildfly-install.sh
 
 chmod 777 /tmp/wildfly-install.sh
 sudo /bin/bash /tmp/wildfly-install.sh
 
-wget https://raw.githubusercontent.com/technsoftware/technosoftware-install-infra/master/wildfly-init-debian.sh
+sudo wget https://raw.githubusercontent.com/technosoftware/ts-install-infra/master/wildfly-init-debian.sh
 sudo cp /tmp/wildfly-init-debian.sh /etc/init.d/wildfly
 sudo chown root:root /etc/init.d/wildfly
 sudo dos2unix /etc/init.d/wildfly
